0f3bce686d4c36878d6cbf82239d76ffc157025b
Mohammed Tawfik <Mohamed.Tawfik2@bd.com>
2024-05-09 16:21:00 +0300

    -Implement Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,17 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedInUser = {
+    firstName: "John",
+    lastName: "Doe",
+  };
   return (
-    <main>
-      SIDEBar
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedInUser} />
       {children}
     </main>
   );
